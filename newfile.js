6c1a7bd024f4c49d9f5111ecfc4961f4e87b3348
Ghassan <ghassan.maslamani@gmail.com>
2018-07-02 11:45:45 +0300

    here is my commit #2

--- newfile.js
+++ newfile.js
@@ -1,3 +1,4 @@
 
 
 document.getElementById('paragraph').innerHTML=document.getElementById('paragraph').innerHTML+" yay!";
+document.getElementById('paragraph').innerHTML=document.getElementById('paragraph').innerHTML+" yay!";
